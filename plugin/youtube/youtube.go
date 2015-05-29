5912cf18076681be101e35416f135fa36775feba
Stephen D. Strowes <sdstrowes@gmail.com>
2015-05-29 21:56:13 +0000

    remove old code that probably doesn't do anything

--- plugin/youtube/youtube.go
+++ plugin/youtube/youtube.go
@@ -4,16 +4,14 @@ package youtube
 import (
 	"net/http"
 	"errors"
-	"fmt"
 	"github.com/sdstrowes/gesture/core"
 	"log"
 	"math/rand"
-	"net/url"
 	"github.com/google/google-api-go-client/googleapi/transport"
 	"github.com/google/google-api-go-client/youtube/v3"
 )
 
-const developerKey = "nope"
+const developerKey = "AIzaSyCh8xg9OGtyyahP4d5ihLSjVR9Kxdriya8"
 
 func Create(bot *core.Gobot, config map[string]interface{}) {
 	results, ok := config["results"].(float64)
@@ -63,35 +61,3 @@ func search(q string, results int64) (link string, err error) {
 
 	return
 }
-
-// Generate a search URL for the given query. Returns the requested number of
-// search results.
-func buildSearchUrl(query string, results int) string {
-	escapedQuery := url.QueryEscape(query)
-	searchString := "https://gdata.youtube.com/feeds/api/videos?q=%v&max-results=%d&v=2&alt=jsonc"
-	return fmt.Sprintf(searchString, escapedQuery, results)
-}
-
-// YouTube response types for deserializing JSON
-type youTubePlayer struct {
-	Default string
-	Mobile  string
-}
-
-type youTubeItem struct {
-	Title       string
-	Description string
-	Player      youTubePlayer
-}
-
-type youTubeData struct {
-	Items []youTubeItem
-}
-
-type youTubeResponse struct {
-	ApiVersion string
-	Data       youTubeData
-}
-
-
-

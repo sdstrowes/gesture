d89c287ffca98e6ecfd3ddec68fed25dfa48f603
Stephen D. Strowes <sdstrowes@gmail.com>
2018-01-01 12:59:24 +0100

    add a festive message

--- plugin/identity/identity.go
+++ plugin/identity/identity.go
@@ -18,4 +18,10 @@ func Create(bot *core.Gobot, config map[string]interface{}) {
 		msg.Send(fmt.Sprintf("why, hello there %s", msg.User))
 		return bot.Stop()
 	})
+
+	bot.ListenFor(fmt.Sprintf("(?i)(H|h)appy new year(!*|,)) %s", name), func(msg core.Message, matches []string) core.Response {
+		msg.Send(fmt.Sprintf("why, hello there %s", msg.User))
+		return bot.Stop()
+	})
 }
+
